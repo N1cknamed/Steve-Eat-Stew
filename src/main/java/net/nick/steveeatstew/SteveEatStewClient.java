6e22bffeaf60130ff7fab041470f6aca642e33ec
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-03-16 20:34:36 +0100

    Initial commit

---
+++ src/main/java/net/nick/steveeatstew/SteveEatStewClient.java
@@ -0,0 +1,10 @@
+package net.nick.steveeatstew;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SteveEatStewClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
